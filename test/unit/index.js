55a6ca0d38d26f7284af442618c9a0ccfd0ee7e6
Kamal Khatwani <kamalkhatwani69@gmail.com>
2023-07-28 20:17:13 +0530

    Additional tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,19 +1,59 @@
+/* eslint-disable no-undef */
 const chai = require('chai');
+const expect = chai.expect;
+const nock = require('nock');
 const HttpClient = require('../../src/http/client');
 
-const expect = chai.expect;
+describe('HttpClient', function() {
+	let httpClient;
 
-describe('HttpClient', () => {
-	it('should make an HTTP request', async () => {
-		const client = new HttpClient();
-		const request = {
-			method: 'get',
-			url: 'http://example.com'
-		};
+	beforeEach(function() {
+		httpClient = new HttpClient();
+	});
 
-		const response = await client.send(request);
+	it('should send text correctly', async function() {
+		nock('http://example.com')
+			.post('/text', 'hello')
+			.reply(200);
+
+		let response = await httpClient.send({
+			method: 'post',
+			url: 'http://example.com/text',
+			data: 'hello'
+		});
 
 		expect(response.status).to.equal(200);
-		expect(response.data).to.be.a('string');
 	});
+
+	it('should send JSON correctly', async function() {
+		nock('http://example.com')
+			.post('/json', { key: 'value' })
+			.reply(200);
+
+		let response = await httpClient.send({
+			method: 'post',
+			url: 'http://example.com/json',
+			data: { key: 'value' }
+		});
+
+		expect(response.status).to.equal(200);
+	});
+
+	it('should send binary data correctly', async function() {
+		let buffer = Buffer.from([0x01, 0x02, 0x03, 0x04]);
+
+		nock('http://example.com')
+			.post('/binary', buffer)
+			.reply(200);
+
+		let response = await httpClient.send({
+			method: 'post',
+			url: 'http://example.com/binary',
+			data: buffer
+		});
+
+		expect(response.status).to.equal(200);
+	});
+
+	// Add more tests here for receiving data...
 });
