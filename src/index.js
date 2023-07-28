be46ed1dabadfc5053ef3b4c39fd5b178eba89d0
Kamal Khatwani <kamalkhatwani69@gmail.com>
2023-07-28 18:24:53 +0530

    Rate limiters, errors, interceptors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 const axios = require('axios');
+const Bottleneck = require('bottleneck');
 
 class HttpClient {
   constructor() {
     this.client = axios.create();
+
+    // Create a new rate limiter
+    this.limiter = new Bottleneck({
+      minTime: 200, // Minimum time between requests
+    });
+
+    // Add interceptors
+    this.client.interceptors.request.use(request => {
+      console.log(`Sending request to ${request.url}`);
+      return request;
+    });
+
+    this.client.interceptors.response.use(response => {
+      console.log(`Received response from ${response.config.url}`);
+      return response;
+    });
+  }
+
+  setDefaultHeaders(headers) {
+    this.client.defaults.headers.common = {...this.client.defaults.headers.common, ...headers};
+  }
+
+  setAuth(token) {
+    this.setDefaultHeaders({ Authorization: `Bearer ${token}` });
   }
 
   async send(request) {
     try {
-      const response = await this.client(request);
+      // Wrap the request in the limiter
+      const response = await this.limiter.schedule(() => this.client(request));
       return response;
     } catch (error) {
       console.error(`Error: ${error.message}`);
