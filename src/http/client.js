ddb1c12461e91897128e1ef4a886959556d02d02
Kamal Khatwani <kamalkhatwani69@gmail.com>
2023-07-28 18:45:34 +0530

    Linting setup

--- src/http/client.js
+++ src/http/client.js
@@ -2,44 +2,44 @@ const axios = require('axios');
 const Bottleneck = require('bottleneck');
 
 class HttpClient {
-  constructor() {
-    this.client = axios.create();
+	constructor() {
+		this.client = axios.create();
 
-    // Create a new rate limiter
-    this.limiter = new Bottleneck({
-      minTime: 200, // Minimum time between requests
-    });
+		// Create a new rate limiter
+		this.limiter = new Bottleneck({
+			minTime: 200, // Minimum time between requests
+		});
 
-    // Add interceptors
-    this.client.interceptors.request.use(request => {
-      console.log(`Sending request to ${request.url}`);
-      return request;
-    });
+		// Add interceptors
+		this.client.interceptors.request.use(request => {
+			console.log(`Sending request to ${request.url}`);
+			return request;
+		});
 
-    this.client.interceptors.response.use(response => {
-      console.log(`Received response from ${response.config.url}`);
-      return response;
-    });
-  }
+		this.client.interceptors.response.use(response => {
+			console.log(`Received response from ${response.config.url}`);
+			return response;
+		});
+	}
 
-  setDefaultHeaders(headers) {
-    this.client.defaults.headers.common = {...this.client.defaults.headers.common, ...headers};
-  }
+	setDefaultHeaders(headers) {
+		this.client.defaults.headers.common = {...this.client.defaults.headers.common, ...headers};
+	}
 
-  setAuth(token) {
-    this.setDefaultHeaders({ Authorization: `Bearer ${token}` });
-  }
+	setAuth(token) {
+		this.setDefaultHeaders({ Authorization: `Bearer ${token}` });
+	}
 
-  async send(request) {
-    try {
-      // Wrap the request in the limiter
-      const response = await this.limiter.schedule(() => this.client(request));
-      return response;
-    } catch (error) {
-      console.error(`Error: ${error.message}`);
-      throw error;
-    }
-  }
+	async send(request) {
+		try {
+			// Wrap the request in the limiter
+			const response = await this.limiter.schedule(() => this.client(request));
+			return response;
+		} catch (error) {
+			console.error(`Error: ${error.message}`);
+			throw error;
+		}
+	}
 }
 
 module.exports = HttpClient;
