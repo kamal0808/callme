0adc341f4b711c07a918f7d5f734e09b4a3a973c
Kamal Khatwani <kamalkhatwani69@gmail.com>
2023-07-28 19:25:43 +0530

    JSDOC

--- src/http/client.js
+++ src/http/client.js
@@ -1,7 +1,14 @@
 const axios = require('axios');
 const Bottleneck = require('bottleneck');
 
+/**
+ * HttpClient is a class for making HTTP requests.
+ * It has built-in features for rate limiting and error handling.
+ */
 class HttpClient {
+	/**
+   * The HttpClient constructor.
+   */
 	constructor() {
 		this.client = axios.create();
 
@@ -22,14 +29,32 @@ class HttpClient {
 		});
 	}
 
+	/**
+   * Set default headers for all requests.
+   * @param {Object} headers - An object representing the default headers.
+   */
 	setDefaultHeaders(headers) {
-		this.client.defaults.headers.common = {...this.client.defaults.headers.common, ...headers};
+		this.client.defaults.headers.common = { ...this.client.defaults.headers.common, ...headers };
 	}
 
+	/**
+   * Set the Authorization header for all requests.
+   * @param {string} token - The token to be included in the Authorization header.
+   */
 	setAuth(token) {
 		this.setDefaultHeaders({ Authorization: `Bearer ${token}` });
 	}
 
+	/**
+   * Send an HTTP request.
+   * @param {Object} request - The request configuration.
+   * @param {string} request.method - The HTTP method (e.g., 'get', 'post').
+   * @param {string} request.url - The URL to request.
+   * @param {Object} [request.headers] - The headers to include in the request.
+   * @param {Object} [request.params] - The query parameters to include in the request.
+   * @param {Object} [request.data] - The body of the request.
+   * @returns {Promise<Object>} - The response from the server.
+   */
 	async send(request) {
 		try {
 			// Wrap the request in the limiter
